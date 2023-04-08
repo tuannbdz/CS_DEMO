baba3dd4077bac854245aab68f13c8f77a827981
Tuan <88710207+tuannbdz@users.noreply.github.com>
2023-04-08 17:19:29 +0700

    Add files via upload

---
+++ Client.cpp
@@ -0,0 +1,145 @@
+﻿#undef UNICODE
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+// Khắc phục lỗi linker
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+//
+#define DEFAULT_PORT "27015"
+#define ADDR "127.0.0.1"
+
+//
+#define DEFAULT_BUFLEN 512
+
+//
+char recvbuf[DEFAULT_BUFLEN];
+int recvbuflen = DEFAULT_BUFLEN;
+
+using namespace std;
+
+int main() {
+    WSADATA wsaData;
+    addrinfo* result = NULL, * ptr = NULL, hints;
+    int iResult;
+
+    //
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+
+    //
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    //
+    iResult = getaddrinfo(ADDR, DEFAULT_PORT, &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return -1;
+    }
+
+    SOCKET connectSocket = INVALID_SOCKET;
+    // Attempt to connect to an address until one succeeds
+    for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+        // Create a SOCKET for connecting to server
+        connectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (connectSocket == INVALID_SOCKET) {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            WSACleanup();
+            return -1;
+        }
+
+        // Connect to server.
+        iResult = connect(connectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(connectSocket);
+            connectSocket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (connectSocket == INVALID_SOCKET) {
+        printf("Unable to connect to server!\n");
+        WSACleanup();
+        return -1;
+    }
+
+    do {
+        cout << "Enter the amount of money you want to deposit (Enter a non-positive number to exit): ";
+        int amount;
+        cin >> amount;
+        if (amount <= 0) {
+            break;
+        }
+
+        string sendbuf = to_string(amount);
+        cout << sendbuf << '\n';
+        iResult = send(connectSocket, sendbuf.c_str(), (int)sendbuf.size(), 0);
+        if (iResult == SOCKET_ERROR) {
+            printf("send failed with error: %d\n", WSAGetLastError());
+            closesocket(connectSocket);
+            WSACleanup();
+            return -1;
+        }
+
+        iResult = recv(connectSocket, recvbuf, recvbuflen, 0);
+        if (iResult > 0) {
+            cout << recvbuf << '\n';
+        }
+        else if (iResult == 0) {
+            cout << "Connection closed\n";
+            closesocket(connectSocket);
+            WSACleanup();
+            return 0;
+        }
+        else {
+            cout << "recv failed with error: " << WSAGetLastError() << '\n';
+            closesocket(connectSocket);
+            WSACleanup();
+            return -1;
+        }
+    } while (iResult > 0);
+
+    //
+    iResult = shutdown(connectSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown failed with error: %d\n", WSAGetLastError());
+        closesocket(connectSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    //
+    do {
+        iResult = recv(connectSocket, recvbuf, recvbuflen, 0);
+        if (iResult > 0)
+            printf("Bytes received: %d\n", iResult);
+        else if (iResult == 0)
+            printf("Connection closed\n");
+        else
+            printf("recv failed with error: %d\n", WSAGetLastError());
+    } while (iResult > 0);
+
+    // cleanup
+    closesocket(connectSocket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ Server.cpp
@@ -0,0 +1,181 @@
+﻿#undef UNICODE
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <iostream>
+#include <thread>
+#include <cstring>
+#include <mutex>
+// Khắc phục lỗi linker
+#pragma comment(lib, "Ws2_32.lib")
+// Port cho server
+#define DEFAULT_PORT "27015"
+
+// Chiều dài buffer
+#define DEFAULT_BUFLEN 512
+
+typedef int sem;
+std::mutex _mutex;
+sem s = 1;
+void down() {
+	std::unique_lock<std::mutex> lock(_mutex);
+	while (s <= 0);
+	s--;
+}
+
+void up() {
+	std::unique_lock<std::mutex> lock(_mutex);
+	s++;
+}
+
+int balance = 1886;
+//
+void CS(int depositAmount) {
+	down();
+	balance -= depositAmount;
+	std::cout << "Balance left: " << balance << '\n';
+	up();
+}
+const char success[] = "Deposit successfully!\n";
+const char failure[] = "Not enough balance!\n";
+
+using namespace std;
+
+void handleClient(SOCKET ClientSocket) {
+	int iResult;
+	int iSendResult;
+
+	char recvbuf[DEFAULT_BUFLEN];
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	do {
+		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+		if (iResult > 0) {
+			int depositAmount = atoi(recvbuf);
+			for (int i = 0; i < DEFAULT_BUFLEN; ++i) {
+				recvbuf[i] = '\0';
+			}
+			if (depositAmount < 0) {
+				break;
+			}
+
+			if (depositAmount > balance) {
+				iSendResult = send(ClientSocket, failure, (int)strlen(failure), 0);
+			}
+			else {
+				CS(depositAmount);
+				iSendResult = send(ClientSocket, success, (int)strlen(success), 0);
+			}
+			if (iSendResult == SOCKET_ERROR) {
+				cout << "send failed: " << WSAGetLastError() << '\n';
+				closesocket(ClientSocket);
+				return;
+			}
+
+			if (balance <= 0) {
+				break;
+			}
+		}
+		else if (iResult < 0) {
+			cout << "recv failed: " << WSAGetLastError() << '\n';
+			closesocket(ClientSocket);
+			return;
+		}
+	} while (iResult > 0);
+
+	iResult = shutdown(ClientSocket, SD_SEND);
+
+	if (iResult == SOCKET_ERROR) {
+		cout << "shutdown failed: " << WSAGetLastError() << '\n';
+	}
+
+	closesocket(ClientSocket);
+}
+
+int main() {
+	// Các biến để thiết lập socket
+	WSADATA wsaData;
+	addrinfo* result = nullptr, * ptr = nullptr, hints;
+	int iResult;
+
+	// Khởi tạo Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		cout << "WSAStartup failed: " << iResult << '\n';
+		return -1;
+	}
+
+	// Thiết lập các thông tin của Socket
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	// 
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		cout << "getaddrinfo failed: " << iResult << '\n';
+		WSACleanup();
+		return -1;
+	}
+
+	//
+	SOCKET ListenSocket = INVALID_SOCKET;
+
+	// Tạo một socket để nghe các client
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+	// 
+	if (ListenSocket == INVALID_SOCKET) {
+		cout << "Error at socket(): " << WSAGetLastError() << '\n';
+		freeaddrinfo(result);
+		WSACleanup();
+		return -1;
+	}
+
+	// 
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		cout << "bind failed with error: " << WSAGetLastError() << '\n';
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return -1;
+	}
+
+	// 
+	freeaddrinfo(result);
+
+	//
+	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
+		cout << "Listen failed with error: " << WSAGetLastError() << '\n';
+		closesocket(ListenSocket);
+		WSACleanup();
+		return -1;
+	}
+
+	//
+	SOCKET clientSocket = INVALID_SOCKET;
+
+	while (balance > 0) {
+		clientSocket = accept(ListenSocket, NULL, NULL);
+
+		if (clientSocket == INVALID_SOCKET) {
+			cout << "accept failed: " << WSAGetLastError() << '\n';
+			closesocket(ListenSocket);
+			WSACleanup();
+			return -1;
+		}
+
+		thread clientThread(handleClient, clientSocket);
+		clientThread.detach();
+	}
+
+	WSACleanup();
+
+	return 0;
+}
